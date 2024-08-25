96a8fe48b9eba61842b5543247c949b661f63d81
fisayo-dev <olufisayobadina@gmail.com>
2024-08-25 19:04:59 +0100

    Modified Button component

--- SeekJob/src/components/Button.jsx
+++ SeekJob/src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ children, customStyles = "" , disabled}) => {
   return (
-    <button disabled={disabled} className={`${customStyles} app-bg-color px-4 py-2 text-white font-bold rounded-lg hover-app-color disabled:bg-gray-400 disabled:cursor-not-allowed`}>
+    <button disabled={disabled} className={`${customStyles} app-bg-color px-4 py-2 text-white font-bold rounded-lg hover-app-bg-color disabled:bg-gray-400 disabled:cursor-not-allowed`}>
       {children}
     </button>
   );
