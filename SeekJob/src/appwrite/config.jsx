f898c77c6673a1f4162b4a6f15d969d694b6380b
fisayo-dev <olufisayobadina@gmail.com>
2024-08-27 19:01:53 +0100

    Integrated appwrite account and database

---
+++ SeekJob/src/appwrite/config.jsx
@@ -0,0 +1,11 @@
+import { Client, Databases, Account } from "appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint(import.meta.env.VITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_PROJECT_ID);
+
+const databases = new Databases(client);
+const account = new Account(client);
+export { client, databases, account };
